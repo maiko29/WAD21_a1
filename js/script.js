2c152919bff1d235bcc35bcb4a3f7d2e44ec7734
Janeli <59377174+6unli@users.noreply.github.com>
2021-10-24 13:28:04 +0300

    Task 4 DONE

--- js/script.js
+++ js/script.js
@@ -17,3 +17,40 @@ window.onclick = function (event) {
         }
     }
 }
+
+
+// Dynamic posts with Json and jQuery
+$(function() {
+    $.get("https://maiko29.github.io/WAD21_a1/data.json", function(posts) {
+        for (post of posts) {
+            let divMain = $('<div class="flex-items">');
+
+            let divHeader = $('<div class="flex-items-header">');
+            let divImage = $('<div class="flex-items-image">');
+            let divContent = $('<div class="flex-items-content">');
+            let divUser = $('<div class="flex-items-avatar">');
+
+            let dateTime = $('<p>').text(post.time);
+            let name = $('<p>').text(post.username);
+            let content = $('<p>').text(post.content);
+
+            divUser.append('<img src="images/' + post.avatar + '" alt="User icon" width="32" height="32"/>');
+            divUser.append(name);
+            divHeader.append(divUser);
+            divHeader.append(dateTime);
+
+            if (post.image != null) {
+                divImage.append('<img src="images/' + post.image + '" alt="User uploaded image" width="100%" height="auto"/>');
+            }
+
+            divContent.append(content);
+            divContent.append('<button>&#128077;</button>')
+
+            divMain.append(divHeader);
+            divMain.append(divImage);
+            divMain.append(divContent);
+
+            $('.main-flex-container').append(divMain)
+        }
+    })
+});
