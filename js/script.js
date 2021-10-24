cd626e88f349225206637656b9876409932415ff
maiko29 <36730047+maiko29@users.noreply.github.com>
2021-10-24 21:55:30 +0300

    Task 5 / local JSON
    Task 5

--- js/script.js
+++ js/script.js
@@ -20,6 +20,7 @@ window.onclick = function (event) {
 
 
 // TASK 4
+/*
 $(function() {
     $.get("https://maiko29.github.io/WAD21_a1/json/data_hosted.json", function(posts) {
         for (post of posts) {
@@ -52,5 +53,65 @@ $(function() {
 
             $('.main-flex-container').append(divMain)
         }
+    })
+});
+*/
+
+// Task 5
+$(function() {
+    $.get("/json/data.json", function (records){
+    for (record of records){
+
+        // main div which will hold "subdivs" of rows
+        let divMain = $('<div class="flex-items">');
+
+        //subdiv for first header row
+        let divHeader = $('<div class="flex-items-header">');
+
+        //subdiv for picture row
+        let divImage = $('<div class="flex-items-image">');
+
+        // main text content of a post record
+        let divContent = $('<div class="flex-items-content">');
+
+        // div for avatar
+        let divUser = $('<div class="flex-items-avatar">');
+
+        //put timestamp into between <p> tags
+        let dateTime = $('<p>').text(record.time);
+
+        // put the author name between <p> tags
+        let name = $('<p>').text(record.username);
+
+        //put the post content between <p> tags
+        let content = $('<p>').text(record.content);
+
+        //put avatar image and user name into same div with class="flex-items-avatar"
+        divUser.append('<img src="' + record.avatar + '" alt="User icon" width="32" height="32"/>');
+        divUser.append(name);
+
+        // put header elements into <div class="flex-items-header">
+        divHeader.append(divUser);
+        divHeader.append(dateTime);
+
+        // if there is present the image URL
+        if (record.image != null) {
+            divImage.append('<img src="' + record.image + '" alt="User uploaded image" width="100%" height="auto"/>');
+        }
+
+        // main content into main "row"
+        divContent.append(content);
+
+        //add the like button
+        divContent.append('<button>&#128077;</button>')
+
+        //combine all three layout rows into post container
+        divMain.append(divHeader);
+        divMain.append(divImage);
+        divMain.append(divContent);
+
+        // append the result into webpage
+        $('.main-flex-container').append(divMain)
+    }
     })
 });
